c1712532ee479d46a9df51fd10112cff1f5ffa4b
Prince Turkar <168743619+Princeturkar@users.noreply.github.com>
2026-05-30 21:32:28 +0530

    Fix EntityScan annotation in ExpenseBackendApplication

--- src/main/java/com/expense_tracker/expense_backend/ExpenseBackendApplication.java
+++ src/main/java/com/expense_tracker/expense_backend/ExpenseBackendApplication.java
@@ -3,13 +3,13 @@ package com.expense_tracker.expense_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.expense_tracker")
 @EnableJpaRepositories(basePackages = "com.expense_tracker.expense_trackerRepository")
-@org.springframework.boot.persistence.autoconfigure.EntityScan(basePackages = "com.expense_tracker.expense_trackerEntity")
+@EntityScan(basePackages = "com.expense_tracker.expense_trackerEntity")
 public class ExpenseBackendApplication {
 
     public static void main(String[] args) {
